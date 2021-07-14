aa777ff6bd1ae21e852c57f0a27cae0ef7b77393
Helen Zhang <helenzhang0736@gmail.com>
2021-07-14 13:29:18 +1200

    Refactor code

--- tagChecker.js
+++ tagChecker.js
@@ -1,6 +1,6 @@
 module.exports.tagChecker = function (paragraph) {
-  const patternOpenTag = /^\<[A-Z]\>+$/,
-    patternCloseTag = /^\<\/[A-Z]\>+$/;
+  const patternOpenTag = /^\<[A-Z]\>$/,
+    patternCloseTag = /^\<\/[A-Z]\>$/;
   let stack = [];
   let i = 0;
   while (i <= paragraph.length - 3) {
@@ -12,7 +12,7 @@ module.exports.tagChecker = function (paragraph) {
     if (patternOpenTag.test(next3Letter)) {
       //Check if this is an opening tag, if it is, push it to the stack
       stack.push(next3Letter);
-      i = i + 3;
+      i = i + 3; //Go to the next char after this opening tag
     } else if (patternCloseTag.test(paragraph.substr(i, 4))) {
       //Check if this is a closing tag, if it is, compare with the last opening tag(if exist) in the stack
       const closeTagLetter = paragraph.substr(i + 2, 1);
@@ -20,18 +20,18 @@ module.exports.tagChecker = function (paragraph) {
       if (!lastOpenTag) return `Expected # found </${closeTagLetter}>`;
       const openTagLetter = lastOpenTag.substr(1, 1);
       if (openTagLetter !== closeTagLetter) return `Expected </${openTagLetter}> found </${closeTagLetter}>`;
-      i = i + 4;
+      i = i + 4; //Go to the next char after this closing tag
     } else {
       i++;
     }
   }
-  //Check if the stack is empty, if it is not, that means there are unmacthed opening tags, and there could be more than one.
+  //Check if the stack is empty, if it is not, that means there are unmacthed opening tags.
   if (stack.length === 0) return "Correctly tagged paragraph";
   let result = "";
   while (stack.length > 0) {
     const tag = stack.pop();
     const expectedCloseTagLetter = tag.substr(1, 1);
-    result += `Expected </${expectedCloseTagLetter}> found # \n`;
+    result += `Expected </${expectedCloseTagLetter}> found # `;
   }
   return result;
 };
